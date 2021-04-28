146b36d7ceb3f0dfd241b94f16fbf5df417cd0e3
Mark Hoffmann <mark.hoffmann@homeaware.com>
2021-04-28 15:09:10 -0500

    removed 3.4 compatibility added 3.6, 3.7 and 3.8

--- setup.py
+++ setup.py
@@ -21,7 +21,9 @@ setuptools.setup(
         'Development Status :: 2 - Pre-Alpha',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
